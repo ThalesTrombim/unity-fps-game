2ebb3774aade2da9a01521585a8a6f095d28dd24
thales <thalestr@outlook.com>
2023-01-30 21:07:36 -0300

    feat: finished game

---
+++ Assets/Creator Kit - FPS/MyImages/GameOverFunctions.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverFunctions : MonoBehaviour
+{
+  private void Start()
+  {
+    Invoke("LoadMenu", 4f);
+  }
+
+  private void LoadMenu()
+  {
+    SceneManager.LoadScene("Menu");
+  }
+}
